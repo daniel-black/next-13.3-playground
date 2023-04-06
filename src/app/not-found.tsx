5e074a39440ed1043989dfebec5d57b952bf3540
daniel-black <drblack651@gmail.com>
2023-04-06 17:29:11 -0600

    add global not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="h-full flex justify-center items-center">
+      <section className="p-10 rounded-lg border border-rose-300 bg-rose-200 w-fit space-y-3">
+        <h2 className="text-3xl font-semibold text-rose-900">Page Not Found {':('}</h2>
+        <p className="text-rose-700">Could not find requested resource</p>
+        <Link href='/' className="block w-full text-center py-2 rounded bg-zinc-100 shadow">Return Home</Link>
+      </section>
+    </div>
+  );
+}
